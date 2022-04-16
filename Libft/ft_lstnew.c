6f60c013de033ff94c975747c934ecd926efc7dd
cupatham <chanon.upa@gmail.com>
2022-04-17 00:52:52 +0700

    bonus1

---
+++ Libft/ft_lstnew.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cupatham <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 00:52:00 by cupatham          #+#    #+#             */
+/*   Updated: 2022/04/17 00:52:27 by cupatham         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list		*ft_lstnew(void *content)
+{
+	t_list *list;
+
+	CHECK_MALLOC(list, sizeof(t_list));
+	list->content = content;
+	list->next = NULL;
+	return (list);
+}
