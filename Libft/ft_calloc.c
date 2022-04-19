5afbda70e0c89fe33c2867ffe907543a35ec17bd
cupatham <chanon.upa@gmail.com>
2022-04-20 01:10:36 +0700

    test calloc 2

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: cupatham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:06:32 by cupatham          #+#    #+#             */
-/*   Updated: 2022/04/20 01:06:38 by cupatham         ###   ########.fr       */
+/*   Updated: 2022/04/20 01:10:14 by cupatham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	total;
 	void	*ptr;
 
-	total = number * size;
-	ptr = malloc(total);
-	if (!ptr)
-		return (NULL);
-	ft_bzero(ptr, total);
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (ptr);
+	ft_bzero(ptr, size * count);
 	return (ptr);
 }
