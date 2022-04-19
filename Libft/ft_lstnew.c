41d045cad43d78208e1e6e9f62e3192c1a10a3c7
cupatham <chanon.upa@gmail.com>
2022-04-19 09:38:53 +0700

    update lstnew

--- Libft/ft_lstnew.c
+++ Libft/ft_lstnew.c
@@ -6,15 +6,15 @@
 /*   By: cupatham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 00:52:00 by cupatham          #+#    #+#             */
-/*   Updated: 2022/04/17 00:52:27 by cupatham         ###   ########.fr       */
+/*   Updated: 2022/04/19 09:38:16 by cupatham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list		*ft_lstnew(void *content)
+t_list	*ft_lstnew(void *content)
 {
-	t_list *list;
+	t_list	*list;
 
 	CHECK_MALLOC(list, sizeof(t_list));
 	list->content = content;
