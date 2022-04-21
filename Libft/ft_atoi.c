4a7beaf8db0241d0996bcc4da902072f43d678ca
cupatham <chanon.upa@gmail.com>
2022-04-22 00:33:48 +0700

    fix atoi

--- Libft/ft_atoi.c
+++ Libft/ft_atoi.c
@@ -6,32 +6,44 @@
 /*   By: cupatham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 15:54:49 by cupatham          #+#    #+#             */
-/*   Updated: 2022/04/16 02:40:54 by cupatham         ###   ########.fr       */
+/*   Updated: 2022/04/22 00:33:31 by cupatham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stddef.h"
+#include <limits.h>
+
+int	ft_isspace(char c)
+{
+	if (c == ' ' || c == '\n' || c == '\t')
+		return (1);
+	if (c == '\v' || c == '\f' || c == '\r')
+		return (1);
+	else
+		return (0);
+}
 
 int	ft_atoi(const char *str)
 {
-	size_t				index;
-	int					neg;
-	unsigned long int	res;
+	int				value;
+	unsigned int	n;
 
-	index = 0;
-	neg = 1;
-	res = 0;
-	while (IS_WSPACE(str[index]))
-		index++;
-	if (str[index] == '+' || str[index] == '-')
-		if (str[index++] == '-')
-			neg *= -1;
-	while (ft_isdigit(str[index]))
+	value = 1;
+	n = 0;
+	while (*str && (ft_isspace(*str) == 1))
+		str++;
+	if (*str == '-')
+		value *= -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
 	{
-		res *= 10;
-		res += str[index] - '0';
-		index++;
+		n = (int)(*str - '0') + (n * 10);
+		if (n > 2147483647 && value == 1)
+			return (-1);
+		if (n > 2147483648 && value == -1)
+			return (0);
+		str++;
 	}
-	return (res * neg);
+	return (n * value);
 }
