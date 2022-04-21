fc0c06790470e278fb5c6bc248cef03e2f1a9e08
cupatham <chanon.upa@gmail.com>
2022-04-22 00:50:26 +0700

    add substr

--- Libft/ft_substr.c
+++ Libft/ft_substr.c
@@ -5,29 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cupatham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/01 19:49:45 by cupatham          #+#    #+#             */
-/*   Updated: 2022/04/16 13:08:55 by cupatham         ###   ########.fr       */
+/*   Created: 2022/04/22 00:49:31 by cupatham          #+#    #+#             */
+/*   Updated: 2022/04/22 00:49:53 by cupatham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stddef.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	char	*substr;
+	size_t	s_len;
 	char	*str;
-	size_t	length;
-	size_t	index;
 
-	CHECK_PTR(s);
-	length = ft_strlen(s);
-	CHECK_MALLOC(str, (len * sizeof(char)) + 1);
-	index = 0;
-	while (index < len && index + start < length)
-	{
-		str[index] = s[start + index];
-		index++;
-	}
-	str[index] = '\0';
-	return (str);
+	str = (char *)s;
+	if (!s)
+		return (0);
+	if (ft_strlen(str) < start)
+		return (ft_strdup(""));
+	s_len = ft_strlen(str + start);
+	if (s_len < len)
+		len = s_len;
+	substr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (0);
+	ft_strlcpy(substr, (str + start), len + 1);
+	return (substr);
 }
