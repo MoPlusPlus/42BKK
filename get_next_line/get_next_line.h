63c3b3087866a39827cf4e00ea1a5c93ae15a677
cupatham <chanon.upa@gmail.com>
2022-04-27 17:11:49 +0700

    test c h utils

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,22 +6,25 @@
 /*   By: cupatham <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 13:16:01 by cupatham          #+#    #+#             */
-/*   Updated: 2022/04/27 13:16:17 by cupatham         ###   ########.fr       */
+/*   Updated: 2022/04/27 17:07:19 by cupatham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include "libft/libft.h"
 # include <stdlib.h>
-# include <sys/types.h>
-# include <sys/uio.h>
-# include <unistd.h>
 
-# define BUFF_SIZE	32
-# define MAX_FD		4865
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
 
-int		get_next_line(const int fd, char **line);
+char	*get_next_line(int fd);
+char	*ft_read_to_left_str(int fd, char *left_str);
+char	*ft_strchr(char *s, int c);
+char	*ft_strjoin(char *left_str, char *buff);
+size_t	ft_strlen(char *s);
+char	*ft_get_line(char *left_str);
+char	*ft_new_left_str(char *left_str);
 
 #endif
